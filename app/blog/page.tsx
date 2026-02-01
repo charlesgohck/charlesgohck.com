b206225461f0a1236c8189bb487ffae459dfca2a
Charles Goh <15852016+charlesgohck@users.noreply.github.com>
2026-02-01 11:48:36 +0800

    [BLOG][FIX] Fixed logo font incorrect

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -349,7 +349,7 @@ export default async function BlogPage({
   };
 
   return (
-    <div className="min-h-screen bg-background">
+    <div className="min-h-screen bg-background font-sans antialiased">
       {/* JSON-LD Structured Data */}
       <script
         type="application/ld+json"
